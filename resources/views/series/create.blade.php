281ae162c72f7e9852de6e423122b4f1a4dcf501
karin morais <andradkarin@gmail.com>
2022-07-26 17:37:55 -0300

    Added the csrf for form validation

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,5 +1,6 @@
 <x-layout title="Add new serie">
-    <form action="" method="post">
+    <form action="/series/save" method="post">
+        @csrf
         <div class="mb-2">
             <label for="name" class="form-label">Name</label>
             <input type="text" name="name" id="name" class="form-control">
