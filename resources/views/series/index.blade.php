f6c5b763e8f8c249904821075fecb140a1e8ef4c
karin morais <andradkarin@gmail.com>
2022-07-26 15:20:43 -0300

    Added basic bootstrap style

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout title="Series">
-    <a href="/series/create">Add new serie</a>
+    <a href="/series/create" class="btn btn-dark mb-2">Add new serie</a>
 <ul class="list-group">
     @foreach($series as $serie)
         <li class="list-group-item">{{ $serie }}</li>
