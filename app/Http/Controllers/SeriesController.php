bc7d16e1ad67c6b8cd217821854d22f393c8a39e
karin morais <andradkarin@gmail.com>
2022-07-28 18:21:53 -0300

    FIxed tab identation

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -7,31 +7,31 @@ use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    public function index(Request $request)
-    {
+	public function index(Request $request)
+	{
 
-        $series = Series::query()->orderBy('name')->get();
+		$series = Series::query()->orderBy('name')->get();
 
-        return view('series.index')->with('series', $series);
-    }
+		return view('series.index')->with('series', $series);
+	}
 
-    public function create()
-    {
-        return view('series.create');
-    }
+	public function create()
+	{
+		return view('series.create');
+	}
 
-    public function store(Request $request)
-    {
-        Series::create($request->all());
-        
-        return to_route('series.index');
+	public function store(Request $request)
+	{
+		Series::create($request->all());
+		
+		return to_route('series.index');
 
-    }
+	}
 
-    public function destroy(Request $request)
-    {
-        Series::destroy($request->id);
+	public function destroy(Request $request)
+	{
+		Series::destroy($request->id);
 
-        return to_route('series.index');
-    }
+		return to_route('series.index');
+	}
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title }}</title>
-    <link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>{{ $title }}</title>
+	<link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
 </head>
 <div class="container">
 <body>
-    <h1>{{ $title }}</h1>
-    {{ $slot }}
-    </body>
+	<h1>{{ $title }}</h1>
+	{{ $slot }}
+	</body>
 </div>
 </html>
