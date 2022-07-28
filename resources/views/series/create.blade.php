9ea9196def0592e0794e306d82e3fa1ea08e570a
karin morais <andradkarin@gmail.com>
2022-07-28 17:50:05 -0300

    Fixed identation

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,10 +1,10 @@
 <x-layout title="Add new serie">
-    <form action="{{ route('series.store') }}" method="post">
-        @csrf
-        <div class="mb-2">
-            <label for="name" class="form-label">Name</label>
-            <input type="text" name="name" id="name" class="form-control">
-        </div>
-        <button type="submit" class="btn btn-primary">Add</button>
-    </form>
+	<form action="{{ route('series.store') }}" method="post">
+		@csrf
+		<div class="mb-2">
+			<label for="name" class="form-label">Name</label>
+			<input type="text" name="name" id="name" class="form-control">
+		</div>
+		<button type="submit" class="btn btn-primary">Add</button>
+	</form>
 </x-layout>
