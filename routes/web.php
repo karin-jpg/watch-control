db2289d846845868deb092e96b1dcd33d4705ae4
karin morais <andradkarin@gmail.com>
2022-07-28 15:46:55 -0300

    Gruoped the routes

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ Route::get('/', function () {
     return redirect('/series');
 });
 
-Route::get('/series', [SeriesController::class, 'index']);
-Route::get('/series/create', [SeriesController::class, 'create']);
-Route::post('/series/save', [SeriesController::class, 'store']);
+Route::controller(SeriesController::class)->group(function () {
+    Route::get('/series', 'index');
+    Route::get('/series/create', 'create');
+    Route::post('/series/save', 'store');
+});
+
