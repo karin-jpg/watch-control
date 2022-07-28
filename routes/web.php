94af595fdf968b1608f5d2d7aead8c467df16129
karin morais <andradkarin@gmail.com>
2022-07-28 15:43:05 -0300

    Changed the landing page

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/series');
 });
 
 Route::get('/series', [SeriesController::class, 'index']);
