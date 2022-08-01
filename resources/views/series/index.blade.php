d0e0b7c36b9e43deec02814af190126caca2a715
karin morais <andradkarin@gmail.com>
2022-08-01 17:09:22 -0300

    Added edit button

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -10,13 +10,17 @@
 	@foreach($series as $serie)
 		<li class="list-group-item d-flex justify-content-between align-itens-center">
 			{{ $serie->name }}
-			<form action="{{ route('series.destroy', $serie->id) }}" method="POST">
-				@method('DELETE')
-				@csrf
-				<button class="btn btn-danger btn-sm">
-					X
-				</button>
-			</form>
+
+            <span class="d-flex">
+                <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary btn-sm">Edit</a>
+                <form action="{{ route('series.destroy', $serie->id) }}" class="ms-2" method="POST">
+                    @method('DELETE')
+                    @csrf
+                    <button class="btn btn-danger btn-sm ">
+                        X
+                    </button>
+                </form>
+            </span>
 		</li>
 	@endforeach
 </ul>
