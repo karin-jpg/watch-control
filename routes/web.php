361b97826c45729cf184b930b85f38990fb5e739
karin morais <andradkarin@gmail.com>
2022-08-01 16:54:10 -0300

    Changed param name on destroy route

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Route::controller(SeriesController::class)->group(function () {
 	Route::get('/series', 'index')->name('series.index');
 	Route::get('/series/create', 'create')->name('series.create');
 	Route::post('/series/store', 'store')->name('series.store');
-	Route::delete('/series/destroy/{id}', 'destroy')->name('series.destroy');
+	Route::delete('/series/destroy/{series}', 'destroy')->name('series.destroy');
 });
 
