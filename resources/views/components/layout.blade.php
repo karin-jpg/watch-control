171e3e4d43c4d0ddd679e456c1fefdc01ac92dd2
karin morais <andradkarin@gmail.com>
2022-08-01 19:19:29 -0300

    Added a error list on layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,6 +10,15 @@
 <div class="container">
 <body>
 	<h1>{{ $title }}</h1>
+	@if ($errors->any())
+		<div class="alert alert-danger">
+			<ul>
+				@foreach ($errors->all() as $error)
+					<li>{{ $error }}</li>
+				@endforeach
+			</ul>
+		</div>
+	@endif
 	{{ $slot }}
 	</body>
 </div>
