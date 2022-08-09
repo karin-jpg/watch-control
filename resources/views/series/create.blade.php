b910d43fbfc040e9869ad678d505b829f56d2b9e
karin morais <andradkarin@gmail.com>
2022-08-09 19:31:33 -0300

    Removed the form layout because os the seasons/episodes changes on create

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,3 +1,30 @@
 <x-layout title="New serie">
-    <x-series.form :action="route('series.store')" :name="old('name')" :update="false" />
+	<form action="{{route('series.store')}}" method="post">
+	@csrf
+	<div class="row mb-3">
+		<div class="col-8">
+			<label for="name" class="form-label">Name</label>
+			<input autofocus type="text"
+					name="name"
+					id="name"
+					class="form-control">
+		</div>
+		<div class="col-2">
+            <label for="seasons" class="form-label">Number of Seasons</label>
+            <input type="text"
+                    name="seasons"
+                    id="seasons"
+                    class="form-control">
+        </div>
+		<div class="col-2">
+            <label for="episodes" class="form-label">Number of Episodes</label>
+            <input type="text"
+                    name="episodes"
+                    id="episodes"
+                    class="form-control">
+        </div>
+	</div>
+	<button type="submit" class="btn btn-primary">Add</button>
+</form>
+
 </x-layout>
