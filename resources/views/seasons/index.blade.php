7cae8a20d262c6902e24b857ed70d4aaec848c07
karin morais <andradkarin@gmail.com>
2022-08-16 20:09:49 -0300

    Added a button to season episodes

--- resources/views/seasons/index.blade.php
+++ resources/views/seasons/index.blade.php
@@ -3,9 +3,9 @@
 <ul class="list-group">
 	@foreach($seasons as $season)
 		<li class="list-group-item d-flex justify-content-between align-itens-center">
-			Season {{ $season->number }}
-
-
+			<a href="{{ route('episodes.index', $season->id)}}">
+				Season {{ $season->number }}
+			</a>
 			<span class="badge bg-secondary">
 				{{ $season->episodes->count() }}
 			</span>
