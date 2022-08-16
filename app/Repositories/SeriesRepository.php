c4930f1a362438cbe29d4338bfc2a86e4cf6884a
karin morais <andradkarin@gmail.com>
2022-08-16 18:17:33 -0300

    Created repository class to add series

--- app/Repositories/SeriesRepository.php
+++ app/Repositories/SeriesRepository.php
@@ -1,8 +1,46 @@
 <?php
 
-namespace App\Repository;
+namespace App\Repositories;
+
+use App\Http\Requests\SeriesFormRequest;
+use App\Models\Series;
+use App\Models\Season;
+use App\Models\Episode;
+use Throwable;
+use Illuminate\Support\Facades\DB;
 
 class SeriesRepository
 {
+	public function add(SeriesFormRequest $request): Series
+	{
+
+		return DB::transaction(function () use ($request) {
+			$series = Series::create($request->all());
+			$seasons =[];
+
+			for($currentSeason = 1; $currentSeason <= $request->seasons; $currentSeason++){
+
+				$seasons[] = [
+					"series_id" => $series->id,
+					"number" => $currentSeason
+				];
+			}
+			Season::insert($seasons);
+
+			$episodes = [];
+			foreach ($series->seasons as $season) {
+				for($currentEpisode = 1; $currentEpisode <= $request->episodes; $currentEpisode++){
+					$episodes[] = [
+						"season_id" => $season->id,
+						"number" => $currentEpisode
+					];
+				}
+			}
+
+			Episode::insert($episodes);
+
+			return $series;
+		});
+	}
 
 }
