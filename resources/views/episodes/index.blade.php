c2dad9a677668041fb878492082c870ec31222bd
karin morais <andradkarin@gmail.com>
2022-08-16 20:26:49 -0300

    Added a form to episodes

--- resources/views/episodes/index.blade.php
+++ resources/views/episodes/index.blade.php
@@ -1,13 +1,15 @@
 <x-layout title="Episodes">
 <a href="{{ route('seasons.index', $series->id) }}" class="btn btn-dark mb-2">Seasons of {{$series->name}}</a>
-<ul class="list-group">
-	@foreach($episodes as $episode)
-		<li class="list-group-item d-flex justify-content-between align-itens-center">
-				Episode {{ $episode->number }}
-				<input type="checkbox" name="episodes[]	">
-		</li>
-	@endforeach
-</ul>
-
-<button class="btn btn-primary mt-2 mb-2">Save</button>
+<form method="post">
+	@csrf
+	<ul class="list-group">
+		@foreach($episodes as $episode)
+			<li class="list-group-item d-flex justify-content-between align-itens-center">
+					Episode {{ $episode->number }}
+					<input type="checkbox" name="episodes[]" value="{{ $episode->id }}">
+			</li>
+		@endforeach
+	</ul>
+	<button class="btn btn-primary mt-2 mb-2">Save</button>
+</form>
 </x-layout>
