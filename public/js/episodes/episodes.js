4e2aa79624f85e3c9bb4b7c9705bda81ffd32f50
karin morais <andradkarin@gmail.com>
2022-08-17 19:21:37 -0300

    fixed checked unchedk logic

--- public/js/episodes/episodes.js
+++ public/js/episodes/episodes.js
@@ -1,20 +1,45 @@
-function checkUncheckAllEpisodes(button) {
 
-	let inputCheck = document.querySelector('#allChecked');
-	let isAllChecked = inputCheck.value === "true";
+window.onload = () => {
+	let episodesCheckbox = document.querySelectorAll('.checkbox');
+	let isAllChecked = true;
+
+	episodesCheckbox.forEach(episodeCheckbox => {
+		if(episodeCheckbox.checked == false){
+			isAllChecked = false;
+		}
+	});
+
+	changeButtonCheckboxText(isAllChecked);
+}
+
+function checkUncheckAllEpisodes() {
+
+	let inputIsAllChecked = document.querySelector('#allChecked');
+	let isAllChecked = inputIsAllChecked.value === "true";
 
 	let episodesCheckbox = document.querySelectorAll('.checkbox');
 	episodesCheckbox.forEach(episodeCheckbox => {
 		episodeCheckbox.checked = !isAllChecked;
 	});
 
+	changeButtonCheckboxText(!isAllChecked);
+
+}
+
+
+function changeButtonCheckboxText(isAllChecked) {
+
+	let button = document.querySelector('#checkButton');
+	let inputIsAllChecked = document.querySelector('#allChecked');
+
 	if(isAllChecked) {
-		button.textContent = "Check all";
-		inputCheck.value = "false";
+		button.textContent = "Uncheck all";
+		inputIsAllChecked.value = "true";
 		return;
 	}
 
-	button.textContent = "Uncheck all";
-	inputCheck.value = "true";
+	button.textContent = "Check all";
+	inputIsAllChecked.value = "false";
 	return;
+
 }
