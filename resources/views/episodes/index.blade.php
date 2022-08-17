717a1d6f8a2df70a2ac65b7441515d19d514b50c
karin morais <andradkarin@gmail.com>
2022-08-17 19:21:57 -0300

    Fixed checkbox logic

--- resources/views/episodes/index.blade.php
+++ resources/views/episodes/index.blade.php
@@ -1,16 +1,16 @@
 <x-layout title="Episodes">
 <div class="d-flex justify-content-between"">
 	<a href="{{ route('seasons.index', $series->id) }}" class="btn btn-dark mb-2">Seasons of {{$series->name}}</a>
-	<button class="btn btn-dark mb-2" onclick="checkUncheckAllEpisodes(this)">Check all</button>
+	<button class="btn btn-dark mb-2" id="checkButton" onclick="checkUncheckAllEpisodes()">Check All</button>
 </div>
 <form method="post">
 	@csrf
 	<ul class="list-group">
-		<input type="hidden" id="allChecked" value="false">
+		<input type="hidden" id="allChecked" value="true">
 		@foreach($episodes as $episode)
 			<li class="list-group-item d-flex justify-content-between align-itens-center">
-					Episode {{ $episode->number }}
-					<input type="checkbox" class="checkbox" name="episodes[]" value="{{ $episode->id }}">
+			 	Episode {{ $episode->number }}
+				<input type="checkbox" class="checkbox" name="episodes[]" value="{{ $episode->id }}" @if($episode->watched) checked @endif />
 			</li>
 		@endforeach
 	</ul>
