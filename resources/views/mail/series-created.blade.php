1e2f145d6de904b344aabc16363259ba07e6bdf0
karin morais <andradkarin@gmail.com>
2022-08-31 20:26:05 -0300

    Added a message on the email view

--- resources/views/mail/series-created.blade.php
+++ resources/views/mail/series-created.blade.php
@@ -1,5 +1,13 @@
 @component('mail::message')
 
-# A new series has been created!
+# A new series has been added!
+
+{{$seriesName}} with {{$seasonsCount}} and {{$episodesCount}} has been created
+
+You can see it right here:
+
+@component('mail::button', ['url' => route('seasons.index', $seriesId)])
+	See more
+@endcomponent
 
 @endcomponent
