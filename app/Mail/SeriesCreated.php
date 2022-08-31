fc5f894d128435849fd27f8f39a568ef51d3f058
karin morais <andradkarin@gmail.com>
2022-08-31 19:32:09 -0300

    Added the view for the email

--- app/Mail/SeriesCreated.php
+++ app/Mail/SeriesCreated.php
@@ -28,6 +28,6 @@ class SeriesCreated extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->view('mail.series-created');
     }
 }
